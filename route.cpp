661fabc6cf85f68bb047dc6746da6f7e576cb086
Jon Powers <powersjo@mail.gvsu.edu>
2015-11-13 08:39:16 -0500

    final copy before presentation

--- route.cpp
+++ route.cpp
@@ -1,3 +1,14 @@
+/*
+ * Project 4 Router.cpp 
+ * CIS 457
+ * 11/13/2015
+ * Author(s):
+ * Jonathan Powers, Kevin Anderson, Brett Greenman
+ * Description:
+ * This program recieves packets and knows if it is an ARP request and then sends an 
+ * ethernet packet back. 
+ */
+
 #include <sys/socket.h> 
 #include <netpacket/packet.h> 
 #include <net/ethernet.h>
@@ -11,8 +22,6 @@
 #include <iomanip>
 #include <linux/if_ether.h>
 #include <netinet/if_ether.h>
-//#include <>
-//#include <>
 
 int main(){
   int packet_socket;
@@ -25,33 +34,19 @@ int main(){
   }
   //have the list, loop over the list
   for(tmp = ifaddr; tmp!=NULL; tmp=tmp->ifa_next){
-    //Check if this is a packet address, there will be one per
-    //interface.  There are IPv4 and IPv6 as well, but we don't care
-    //about those for the purpose of enumerating interfaces. We can
-    //use the AF_INET addresses in this list for example to get a list
-    //of our own IP addresses
+    
     if(tmp->ifa_addr->sa_family==AF_PACKET){
       printf("Interface: %s\n",tmp->ifa_name);
       //create a packet socket on interface r?-eth1
       if(!strncmp(&(tmp->ifa_name[3]),"eth1",4)){
 	printf("Creating Socket on interface %s\n",tmp->ifa_name);
 	//create a packet socket
-	//AF_PACKET makes it a packet socket
-	//SOCK_RAW makes it so we get the entire packet
-	//could also use SOCK_DGRAM to cut off link layer header
-	//ETH_P_ALL indicates we want all (upper layer) protocols
-	//we could specify just a specific one
 	packet_socket = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
 	if(packet_socket<0){
 	  perror("socket");
 	  return 2;
 	}
-	//Bind the socket to the address, so we only get packets
-	//recieved on this specific interface. For packet sockets, the
-	//address structure is a struct sockaddr_ll (see the man page
-	//for "packet"), but of course bind takes a struct sockaddr.
-	//Here, we can use the sockaddr we got from getifaddrs (which
-	//we could convert to sockaddr_ll if we needed to)
+	
 	if(bind(packet_socket,tmp->ifa_addr,sizeof(struct sockaddr_ll))==-1){
 	  perror("bind");
 	}
@@ -61,30 +56,18 @@ int main(){
   //free the interface list when we don't need it anymore
   freeifaddrs(ifaddr);
 
-  //loop and recieve packets. We are only looking at one interface,
-  //for the project you will probably want to look at more (to do so,
-  //a good way is to have one socket per interface and use select to
-  //see which ones have data)
   printf("Ready to recieve now\n");
   while(1){
     char buf[1500];
     struct sockaddr_ll recvaddr;
     socklen_t recvaddrlen=sizeof(struct sockaddr_ll);
-    //we can use recv, since the addresses are in the packet, but we
-    //use recvfrom because it gives us an easy way to determine if
-    //this packet is incoming or outgoing (when using ETH_P_ALL, we
-    //see packets in both directions. Only outgoing can be seen when
-    //using a packet socket with some specific protocol)
+    
     int n = recvfrom(packet_socket, buf, 1500,0,(struct sockaddr*)&recvaddr, &recvaddrlen);
-    //ignore outgoing packets (we can't disable some from being sent
-    //by the OS automatically, for example ICMP port unreachable
-    //messages, so we will just ignore them here)
+    
     if(recvaddr.sll_pkttype==PACKET_OUTGOING){
       continue;
     }
-    //start processing all others
-    //printf("Got a %d byte packet\n", n);
-
+    
       if ((((buf[12]) << 8) + buf[13]) == ETH_P_ARP){
 		printf("Recieved ARP request\n");
 	}	
@@ -125,16 +108,7 @@ int main(){
 	if (sendto(packet_socket,&requ,sizeof(requ),0,(struct sockaddr*)&device,sizeof(device)) < 0) {
     		printf("%s",strerror(errno));
 	}
-
-    //what else to do is up to you, you can send packets with send,
-    //just like we used for TCP sockets (or you can use sendto, but it
-    //is not necessary, since the headers, including all addresses,
-    //need to be in the buffer you are sending)
-    
   }
-
-
-
   //exit
   return 0;
 }
